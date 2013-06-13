1af235c88cfef49acc81ae6d5b1619d34bc46e7c
Gustavo Vinicius <gustavovnicius@hotmail.com>
2013-06-13 15:53:09 -0300

    Basic test. .gitignore refactoring

---
+++ SimplePresenterAppTest/src/br/eti/matheus/simplepresenter/test/SimpleTest.java
@@ -0,0 +1,14 @@
+package br.eti.matheus.simplepresenter.test;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class SimpleTest extends TestCase {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
