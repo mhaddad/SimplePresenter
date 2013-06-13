79b8f5d2c008baabe7125ba38af1c9c39654fcb2
Gustavo Vinicius <gustavovnicius@hotmail.com>
2013-06-13 11:58:39 -0300

    Created simple test inside package br.eti.matheus.simplepresenter.tests

---
+++ SimplePresenterApp/src/br/eti/matheus/simplepresenter/tests/SimpleTest.java
@@ -0,0 +1,14 @@
+package br.eti.matheus.simplepresenter.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		assertEquals(true, true);
+	}
+
+}
